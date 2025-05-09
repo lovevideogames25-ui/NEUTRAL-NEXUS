59b2180007c5bbe97751e60d317035dc5be58764
technonyte dev <technonytealternate+1@outlook.com>
2025-05-09 13:45:36 +0000

    change "games" to "gầmes"

--- scripts/navi.js
+++ scripts/navi.js
@@ -4,10 +4,10 @@
 
 const navItems = [
   { title: "Home", icon: "fa-home", url: "page/home.html" },
-  { title: "Games", icon: "fa-gamepad-modern", url: "page/games.html" },
+  { title: "Gầmes", icon: "fa-gamepad-modern", url: "page/games.html" },
   { title: "VM", icon: "fa-desktop", url: "page/vm.html" },
-  { title: "Media", icon: "fa-photo-film", url: "page/media.html" },
-  { title: "Chat [BETA]", icon: "fa-comments-alt", url: "page/chat.html" },
-  { title: "Browser", icon: "fa-search", url: "page/browse.html" },
+  { title: "Chat", icon: "fa-comments-alt", url: "page/chat.html" },
+  { title: "Movies", icon: "fa-clapperboard", url: "page/mov.html" },
   { title: "AI", icon: "fa-robot", url: "page/ai.html" },
+  { title: "Browser", icon: "fa-search", url: "page/browse.html" },
 ];
