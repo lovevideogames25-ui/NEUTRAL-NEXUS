bf65e429ceb2764f223442936e816beb4004b839
technonyte dev <technonytealternate+1@outlook.com>
2025-05-22 13:26:55 +0000

    v3.13

--- scripts/navi.js
+++ scripts/navi.js
@@ -6,8 +6,8 @@ const navItems = [
   { title: "Home", icon: "fa-home", url: "page/home.html" },
   { title: "Games", icon: "fa-gamepad-modern", url: "page/games.html" },
   { title: "VM", icon: "fa-desktop", url: "page/vm-priv.html" },
+  { title: "Browser", icon: "fa-search", url: "/scram.html" },
   { title: "Apps", icon: "fa-grid", url: "page/apps.html" },
   { title: "Chat", icon: "fa-comments-alt", url: "page/chat.html" },
   { title: "Movies", icon: "fa-clapperboard", url: "page/mov.html" },
-  { title: "Browser", icon: "fa-search", url: "page/browse.html" },
 ];
