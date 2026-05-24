1f0b5e3484088c04087c75ad4b980f56810893ae
Your Name <lovevideogames25@gmail.com>
2026-05-24 09:41:57 -0500

    Remove scramjet.wasm.wasm from build.js and .gitignore

--- build.js
+++ build.js
@@ -1,13 +1,38 @@
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
 
-// Copy scramjet.wasm.wasm from scram directory to root
-const sourcePath = path.join(__dirname, 'scram', 'scramjet.wasm.wasm');
-const destPath = path.join(__dirname, 'scramjet.wasm.wasm');
-
-if (fs.existsSync(sourcePath)) {
-  fs.copyFileSync(sourcePath, destPath);
-  console.log('Copied scramjet.wasm.wasm to root directory');
-} else {
-  console.error('scramjet.wasm.wasm not found in scram directory');
+// Ensure webfonts directory exists
+const webfontsDir = path.join(__dirname, '_a', 'fa', 'webfonts');
+if (!fs.existsSync(webfontsDir)) {
+  fs.mkdirSync(webfontsDir, { recursive: true });
+  console.log('Created webfonts directory');
 }
+
+// Download Font Awesome web fonts if they don't exist
+const fontFiles = [
+  'fa-brands-400.woff2',
+  'fa-regular-400.woff2',
+  'fa-solid-900.woff2'
+];
+
+const fontBaseUrl = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/webfonts/';
+
+fontFiles.forEach(fontFile => {
+  const fontPath = path.join(webfontsDir, fontFile);
+  if (!fs.existsSync(fontPath)) {
+    const file = fs.createWriteStream(fontPath);
+    https.get(`${fontBaseUrl}${fontFile}`, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close();
+        console.log(`Downloaded ${fontFile}`);
+      });
+    }).on('error', (err) => {
+      console.error(`Error downloading ${fontFile}:`, err);
+      fs.unlink(fontPath, () => {});
+    });
+  } else {
+    console.log(`${fontFile} already exists`);
+  }
+});
